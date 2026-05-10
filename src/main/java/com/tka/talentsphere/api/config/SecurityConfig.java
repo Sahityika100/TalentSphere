61000ab46db05887ed610f90305d930572a524b9
Sahityika100 <sahityikachoudhari19@gmail.com>
2026-05-10 23:07:14 +0530

    Update Changes

--- src/main/java/com/tka/talentsphere/api/config/SecurityConfig.java
+++ src/main/java/com/tka/talentsphere/api/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173","https://talent-sphere-frontend-tau.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://talent-sphere-frontend-mu.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
